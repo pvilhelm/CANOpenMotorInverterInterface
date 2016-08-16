dc8859fd63a19cab9f4e40b151f29671fbb35ac9
fisksoppa <fisksoppa@130.229.167.84>
2016-08-16 17:22:56 +0200

    La till en massa grejer till

--- src/motorinvertercontroller/CANOpenMessageSDO.java
+++ src/motorinvertercontroller/CANOpenMessageSDO.java
@@ -6,6 +6,9 @@
 package motorinvertercontroller;
 
 import core.Canlib;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
 import obj.CanlibException;
 import obj.Handle;
 import obj.Message;
@@ -28,18 +31,96 @@ public class CANOpenMessageSDO extends CANOpenMessage{
     CANOpenMessageSDO(){
         
     }
-    
-    boolean setSDOdata(byte[] data, int index, int subindex){
-        if(data.length>6)
-            return false; 
-        if(index> 1<<16)
-            return false;
-        if(subindex > 255)
-            return false; 
-        
-        else{
-            this.data = new byte[data.length+2];
+    byte[] getSDOData() throws CANInterfaceException{
+        int cmd = getSDOCmd();
+        switch(cmd){
+            case(0x4f):
+                if(this.data.length<5)
+                    throw new CANInterfaceException("CAN SDO received message data length out of spec.");
+                return Arrays.copyOfRange(this.getData(),4,5);
+            case(0x4b):
+                if(this.data.length<6)
+                    throw new CANInterfaceException("CAN SDO received message data length out of spec.");
+                return Arrays.copyOfRange(this.getData(),4,6);
+            case(0x47):
+                if(this.data.length<7)
+                    throw new CANInterfaceException("CAN SDO received message data length out of spec.");
+                return Arrays.copyOfRange(this.getData(),4,7);
+            case(0x43):
+                if(this.data.length<8)
+                    throw new CANInterfaceException("CAN SDO received message data length out of spec.");
+                return Arrays.copyOfRange(this.getData(),4,8);
+            case(0x2f):
+                if(this.data.length<5)
+                    throw new CANInterfaceException("CAN SDO received message data length out of spec.");
+                return Arrays.copyOfRange(this.getData(),4,5);
+            case(0x2b):
+                if(this.data.length<6)
+                    throw new CANInterfaceException("CAN SDO received message data length out of spec.");
+                return Arrays.copyOfRange(this.getData(),4,6);
+            case(0x27):
+                if(this.data.length<7)
+                    throw new CANInterfaceException("CAN SDO received message data length out of spec.");
+                return Arrays.copyOfRange(this.getData(),4,7);
+            case(0x23):
+                if(this.data.length<8)
+                    throw new CANInterfaceException("CAN SDO received message data length out of spec.");
+                return Arrays.copyOfRange(this.getData(),4,8);
+            case(0x40):case(0x22):case(60):
+                if(this.data.length<5)
+                    throw new CANInterfaceException("CAN SDO received message data length out of spec.");
+                return Arrays.copyOfRange(this.getData(),4,Math.min(8,this.data.length));  
+            default:
+                throw new CANInterfaceException("CAN SDO received message command byte out of spec.");
         }
+    }
+    
+    byte getSDOCmd(){
+        return data[0];
+    }
+    
+    void setSDOdataToSend(byte[] data, int index, int subindex, boolean write) throws CANInterfaceException {
+        if(data == null || !write)
+            data = new byte[0];
+        if(data.length>4)
+            throw new CANInterfaceException("SDO Data length over 4b"); 
+        if(index> ~((~0)<<16))
+            throw new CANInterfaceException("SDO Index not short"); 
+        if(subindex > 255)
+            throw new CANInterfaceException("SDO Sub-index not byte"); 
+        
+ 
+        {
+            this.data = new byte[data.length+4];
+            ByteBuffer dataBuffer = ByteBuffer.wrap(this.data);
+            dataBuffer.order(ByteOrder.LITTLE_ENDIAN);
+            if(write){
+                
+                switch(data.length){
+                    case(1):
+                        dataBuffer.put((byte)0x2f);
+                        break;
+                    case(2):
+                        dataBuffer.put((byte)0x2b);
+                        break;
+                    case(4):
+                        dataBuffer.put((byte)0x23);
+                        break;
+                    default:
+                        throw new CANInterfaceException("SDO Write data length out of spec. Length:"+data.length); 
+                }
+            }
             
+            else{
+                dataBuffer.put((byte)0x40);
+            }
+            
+            dataBuffer.putChar((char)index);
+            dataBuffer.put((byte)subindex);
+            if(write)
+                dataBuffer.put(data);
+            
+            return;
+        }
     }
 }
--- src/motorinvertercontroller/MotorInverterController.java
+++ src/motorinvertercontroller/MotorInverterController.java
@@ -5,6 +5,10 @@
  */
 package motorinvertercontroller;
 import core.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import obj.*;
@@ -18,12 +22,24 @@ public class MotorInverterController {
      * @param args the command line arguments
      */
     public static void main(String[] args)  {
+        try {
+            File file = new File("./test.json");
+            FileOutputStream out = new FileOutputStream(file);
+            out.write('a');
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MotorInverterController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MotorInverterController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         CANdevice kvDevice = new KvaserDevice();
         try {
             kvDevice.init(0);
             kvDevice.goOnBus();
             Message test = new Message(0,new byte[8],8,0);
+            CANOpenMessageSDO test2 = new CANOpenMessageSDO();
+            test2.setSDOdataToSend(new byte[]{0,4}, 10, 1, true);
             kvDevice.sendMessageBlocking(test);
+            kvDevice.sendMessage(test2.getAsMessage());
         } catch (CANInterfaceException ex) {
             Logger.getLogger(MotorInverterController.class.getName()).log(Level.SEVERE, null, ex);
         }  
